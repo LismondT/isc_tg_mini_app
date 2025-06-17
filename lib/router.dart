975c6f88fdb8f17fbabc04f20c14bf8aa4a41f1f
LismondT <moonfox923@gmail.com>
2025-06-17 18:58:34 +0700

    hz

--- lib/router.dart
+++ lib/router.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:tg_mini_app/main.dart';
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
+// import 'package:tg_mini_app/main.dart';
 
-final router = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const LevelSelectionScreen(),
-      routes: [
-        GoRoute(
-          path: 'level/:levelId',
-          builder: (context, state) {
-            final levelId = int.parse(state.pathParameters['levelId']!);
-            return GameScreen(level: levelId, onLevelComplete: (a) => {});
-          },
-        ),
-      ],
-    ),
-  ],
-  errorBuilder: (context, state) =>
-      const Scaffold(body: Center(child: Text('Page not found'))),
-);
+// final router = GoRouter(
+//   initialLocation: '/',
+//   routes: [
+//     GoRoute(
+//       path: '/',
+//       builder: (context, state) => const LevelSelectionScreen(),
+//       routes: [
+//         GoRoute(
+//           path: 'level/:levelId',
+//           builder: (context, state) {
+//             final levelId = int.parse(state.pathParameters['levelId']!);
+//             return GameScreen(level: levelId, onLevelComplete: (a) => {});
+//           },
+//         ),
+//       ],
+//     ),
+//   ],
+//   errorBuilder: (context, state) =>
+//       const Scaffold(body: Center(child: Text('Page not found'))),
+// );
