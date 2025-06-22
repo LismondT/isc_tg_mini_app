e347a285dd14fdab93de94e6f9d58baa3cfd8662
LismondT <moonfox923@gmail.com>
2025-06-22 18:52:47 +0700

    42

--- lib/router.dart
+++ lib/router.dart
@@ -10,7 +10,7 @@ final router = GoRouter(
   redirect: (BuildContext context, GoRouterState state) {
     if (state.uri.toString().contains('WebAppData')) {
       final query = state.uri.queryParameters['tgWebAppData'];
-      return '/debug/$query';
+      return '/debug/${state.uri.toString()}';
     }
     return null;
   },
